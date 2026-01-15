d30a412d5d3c7c088593066c0ec171b24b02ad9a
方块君 <fkj@fkj233.cn>
2026-01-15 19:51:19 +0800

    fix

--- dist-electron/preload.js
+++
@@ -1,7 +0,0 @@
-import { contextBridge, ipcRenderer } from "electron";
-contextBridge.exposeInMainWorld("electron", {
-  minimize: () => ipcRenderer.send("window-minimize"),
-  toggleMaximize: () => ipcRenderer.send("window-maximize"),
-  close: () => ipcRenderer.send("window-close"),
-  openExternal: (url) => ipcRenderer.send("open-external", url)
-});
